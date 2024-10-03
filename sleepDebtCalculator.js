d05e58550b69085cc1f40f12c7db1898e0a53f98
Ronald Pasion <52368740+RonaldPasion@users.noreply.github.com>
2024-10-03 16:36:26 -0700

    Update sleepDebtCalculator.js

--- sleepDebtCalculator.js
+++ sleepDebtCalculator.js
@@ -1,3 +1,25 @@
 function getSleepHours(day) {
-  
+   switch (day) {
+    case 'monday':
+      return 8;
+      break;
+    case 'tuesday':
+      return 6;
+      break;
+    case 'wednesday':
+      return 7;
+      break;
+    case 'thursday':
+      return 4;
+      break;
+    case 'friday':
+      return 9;
+      break;
+    case 'saturday':
+      return 12;
+      break;
+    case 'sunday':
+      return 10;
+      break;
+  }
 }
